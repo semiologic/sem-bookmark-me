f1cd0db99dc1ad3bb44ace9152193192d67285c7
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-22 11:23:53 +0000

    #289 tweaks
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-bookmark-me/trunk@608 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-bookmark-me.php
+++ sem-bookmark-me.php
@@ -3,8 +3,8 @@
 Plugin Name: Bookmark Me
 Plugin URI: http://www.semiologic.com/software/widgets/bookmark-me/
 Description: Adds widgets that lets visitors subscribe your webpages to social bookmarking sites such as del.icio.us and Digg.
-Author: Denis de Bernardy
 Version: 4.5.1
+Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 */
 
