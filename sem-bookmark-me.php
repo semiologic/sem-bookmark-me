849511bb554de3a3fcdcbd90a53f98e5df98c6d8
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-18 11:33:05 +0000

    move scripts to the footer
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-bookmark-me/trunk@2190 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-bookmark-me.php
+++ sem-bookmark-me.php
@@ -110,7 +110,7 @@ class bookmark_me extends WP_Widget {
 
 	function scripts() {
 		$folder = plugin_dir_url(__FILE__);
-		wp_enqueue_script('bookmark_me', $folder . 'js/scripts.js', array('jquery'), '5.0');
+		wp_enqueue_script('bookmark_me', $folder . 'js/scripts.js', array('jquery'), '5.0', true);
 	} # scripts()
 	
 	
